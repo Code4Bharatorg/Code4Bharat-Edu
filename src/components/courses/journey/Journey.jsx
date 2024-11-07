fc5119a62e53b1137cb2684763da2f4fc005bc9a
Sanadn12 <ni.sanad1433@gmail.com>
2024-11-07 16:33:19 +0530

    chnages in

--- src/components/courses/journey/Journey.jsx
+++ src/components/courses/journey/Journey.jsx
@@ -5,11 +5,11 @@ const Journey = () => {
   return (
     <div className="w-full h-auto min-h-[60vh] bg-[#116EB3] flex flex-col md:flex-row items-center md:items-start px-8 mb-8 md:px-24 py-8 gap-6 md:gap-12">
       {/* Text Section */}
-      <div className="w-full md:w-3/5 flex flex-col gap-6 text-white">
+      <div className="w-full md:w-3/5 flex flex-col gap-6 text-white mt-12">
         <h1 className="text-[5vw] md:text-[2.5vw] font-bold leading-tight md:leading-none text-center md:text-left text-black">
           Ready to Start Your <span className="text-white">Journey?</span>
         </h1>
-        <p className="text-[1.5rem] font-normal leading-relaxed text-justify">
+        <p className="text-[1.5rem] font-normal leading-relaxed text-justify mt-12">
           Whether you&apos;re a recent graduate, a working professional, or someone
           looking to switch careers, Education.Code4Bharat has a program for you.
         </p>
